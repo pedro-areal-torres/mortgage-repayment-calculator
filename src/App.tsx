9a1b620626b5824f34f825b9cdec13cce668efed
Pedro Torres <pedro.areal.torres@ing.com>
2024-07-06 12:01:31 +0200

    feat: added calculator page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useTranslation } from 'react-i18next';
 import './App.css';
+import LanguageSwitcher from './components/language-switcher/language-switcher';
 import Calculator from './components/pages/calculator';
+import CalculatorTable from './components/calculator-table/calculator-table';
+import { MortgageCalculationResult } from './utils/calculator.utils';
+import React from 'react';
+
+export interface CalculationResult {
+  costNoRepayments: MortgageCalculationResult;
+  costWithRepayments: MortgageCalculationResult;
+}
 
 function App() {
+  const { t } = useTranslation();
+  const [calculation, setCalculationDetails] =
+    React.useState<CalculationResult>();
+
   return (
-    <div>
-      <Calculator />
+    <div className="min-h-screen py-2 px-4 max-w-3xl m-auto">
+      <div className="flex flex-row gap-2 justify-between px-2 items-center py-2">
+        <h2 className="text-base font-bold leading-7 text-gray-900">
+          {t('Mortgage Simulator')}
+        </h2>
+        <LanguageSwitcher />
+      </div>
+      <div className="bg-white shadow-md ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+        <Calculator setCalculationDetails={setCalculationDetails} />
+      </div>
+      <CalculatorTable calculation={calculation} />
     </div>
   );
 }
---
+++ src/components/calculator-table/calculator-table.tsx
@@ -0,0 +1,157 @@
+import { CalculationResult } from '../../App';
+import { useTranslation } from 'react-i18next';
+import { Separator } from '../ui/separator';
+
+interface Props {
+  calculation?: CalculationResult;
+}
+
+function CalculatorTable({ calculation }: Props) {
+  const { t } = useTranslation();
+
+  /*
+    Inflação: Montante em dívida * 2% * num anos
+
+    */
+
+  return (
+    <>
+      {calculation && (
+        <>
+          <div>
+            <h3 className="text-base font-semibold leading-6 text-gray-900">
+              {t('Mortgage scenarios')}
+            </h3>
+            <dl className="mt-5 grid grid-cols-1 md:grid-cols-2 gap-5">
+              <div className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+                <dt className="truncate text-sm font-medium text-gray-500">
+                  {t('Mortgage Without Repayments')}
+                </dt>
+                <dd className="mt-1 text-md font-normal tracking-tight text-gray-900">
+                  <div>
+                    Custo do Empréstimo:{' '}
+                    {calculation.costNoRepayments.totalCost}
+                  </div>
+                  <div>Amortizações: 0</div>
+                  <div>
+                    Custo Total: {calculation.costNoRepayments.totalCost}
+                  </div>
+                  <Separator className="my-4" />
+                  <div>
+                    Património no final do empréstimo (
+                    {calculation.costNoRepayments.totalMonths} {t('months')})
+                  </div>
+                </dd>
+              </div>
+            </dl>
+          </div>
+          <div>
+            <h2>: ${calculation.costNoRepayments.totalCost}</h2>
+            <h2>{t('Total Cost of Mortgage With Repayments')}: $</h2>
+            <h2>
+              {t('Total Spent on Repayments')}: $
+              {`${calculation.costWithRepayments.repaymentDetails.repaymentsAmount} (${calculation.costWithRepayments.repaymentDetails.repaymentsCount})`}
+            </h2>
+            <h2>
+              {t('Total Earned on S&P')}:
+              {calculation.costWithRepayments.investmentDetails.totalEarnedOnSP}
+            </h2>
+            <h2>
+              {t('Profit Earned on S&P')}:
+              {calculation.costWithRepayments.investmentDetails.totalProfitSP}
+            </h2>
+          </div>
+          <div className="mt-8 flow-root">
+            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-300">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                        >
+                          {t('Month')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Monthly Payment')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Interest Paid')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Principal Paid')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Remaining Debt')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Repayment')}
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                        >
+                          {t('Return on Repayment')}
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="divide-y divide-gray-200 bg-white">
+                      {calculation.costWithRepayments.paymentDetails.map(
+                        (detail) => (
+                          <tr key={detail.month}>
+                            <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                              {detail.month}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {detail.monthlyPayment} €
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {detail.interestPaid} €
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {detail.principalPaid} €
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {detail.remainingDebt} €
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {'-'}
+                            </td>
+                            <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                              {detail.returnOnRepaymentPercentage > 0
+                                ? `${detail.returnOnRepaymentPercentage} %`
+                                : '-'}
+                            </td>
+                          </tr>
+                        )
+                      )}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default CalculatorTable;
