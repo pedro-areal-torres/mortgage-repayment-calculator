b791b8e1275a56d5fceeb129c87fb91eb7e7d433
Pedro Torres <pedro.areal.torres@ing.com>
2024-07-30 13:35:03 +0200

    chore: update translations + app icon

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,19 @@ function App() {
     React.useState<CalculationResult>();
 
   return (
-    <div className="min-h-screen py-2 px-4 max-w-4xl m-auto">
-      <div className="flex flex-row gap-2 justify-between px-2 items-center py-2">
-        <h2 className="text-xl font-bold leading-7 text-gray-900">
-          {t('Repayment vs Investing')}
-        </h2>
-        <LanguageSwitcher />
+    <div className="bg-gray-100">
+      <div className="min-h-screen py-2 px-4 m-auto max-w-4xl">
+        <div className="flex flex-row gap-2 justify-between px-2 items-center py-2">
+          <h2 className="text-xl font-bold leading-7 text-green-500">
+            {t('Repayment vs Investing')}
+          </h2>
+          <LanguageSwitcher />
+        </div>
+        <div className="bg-white shadow-md ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
+          <Calculator setCalculationDetails={setCalculationDetails} />
+        </div>
+        <CalculatorTable calculation={calculation} />
       </div>
-      <div className="bg-white shadow-md ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2">
-        <Calculator setCalculationDetails={setCalculationDetails} />
-      </div>
-      <CalculatorTable calculation={calculation} />
     </div>
   );
 }
