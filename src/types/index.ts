550adce9864611dd95050c910b1f701a2a43aed9
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2025-05-02 11:59:47 +0200

    refactor: update folder structure

---
+++ src/types/index.ts
@@ -0,0 +1,58 @@
+export interface CalculationResult {
+  noAction: MortgageCalculationResult;
+  onlyRepayment: MortgageCalculationResult;
+  onlyInvesting: MortgageCalculationResult;
+  fiftyFifty: MortgageCalculationResult;
+}
+
+export interface MortgageCalculationResult {
+  overview: OverviewDetails;
+  assetsDetails: AssetsDetails;
+  mortgageDetails: MortgageDetails;
+  investmentDetails: InvestmentDetails;
+}
+
+export interface MonthlyPaymentDetails {
+  month: number;
+  monthlyPayment: number;
+  interestPaid: number;
+  principalPaid: number;
+  remainingDebt: number;
+  monthlyPaymentReduction: number;
+  monthlyPaymentSavedInterest: number;
+  totalInterestSavedWithRepayment: number;
+  returnOnRepaymentPercentage: number;
+  lastRepaymentAmount: number;
+}
+
+export interface MortgageDetails {
+  totalMonths: number;
+  totalCost: number;
+  totalInterest: number;
+  totalDebt: number;
+  totalSavedOnInterest: number;
+  estimatedRepayment?: number;
+  monthlyPayments: MonthlyPaymentDetails[];
+  repaymentDetails: RepaymentDetails;
+}
+
+interface OverviewDetails {
+  earned: number;
+  costs: number;
+  net: number;
+}
+
+interface AssetsDetails {
+  houseValue: number;
+  savings: number;
+}
+
+interface RepaymentDetails {
+  amount: number;
+  count: number;
+}
+
+interface InvestmentDetails {
+  profit: number;
+  invested: number;
+}
