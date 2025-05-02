569c58c31ab237fd77a5129a2a3996bed07532da
Pedro Torres <88969553+pedro-areal-torres@users.noreply.github.com>
2025-05-02 11:57:47 +0200

    chore: added eslintrc rules for imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,79 @@ module.exports = {
   extends: ['prettier'],
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    // Your custom rules here
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '@actions/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/constants',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/enums',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/mappings',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/translations',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/types',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/utils',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@lib/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@components/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@core/**',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
   },
   settings: {
     react: {
